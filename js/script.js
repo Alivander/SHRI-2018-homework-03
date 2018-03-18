d1be23dfc43b3ced59fe85d2b11633a3225cac80
Alina Vanieva <vanieva.alina@yandex.ru>
2018-03-18 15:58:54 +0300

    Updates

---
+++ js/script.js
@@ -0,0 +1,73 @@
+(function() {
+  const video = document.querySelector(".video");
+  const canvasFrequency = document.querySelector(".player");
+  const canvasVolume = document.querySelector(".volume__scale");
+  const interfaceColor = "rgb(34, 236, 255)";
+  const interfaceColorRGBA = "rgba(34, 236, 255, 0.15)";
+
+  if (navigator.mediaDevices) {
+    navigator.mediaDevices.getUserMedia({audio: true, video: true})
+      .then(function(stream) {
+        video.srcObject = stream;
+        video.play();
+
+        const AudioContext = window.AudioContext || window.webkitAudioContext;
+        const audioContext = new AudioContext();
+        const source = audioContext.createMediaStreamSource(stream);
+        const destination = audioContext.destination;
+        const filter = audioContext.createBiquadFilter()
+        const analyser = audioContext.createAnalyser();
+
+        filter.type = "lowshelf";
+        filter.frequency.value = 1000;
+        filter.gain.value = 3;
+
+        analyser.fftSize = 256;
+        const bufferLength = analyser.frequencyBinCount;
+        const dataArray = new Uint8Array(bufferLength);
+
+        function draw() {
+          const canvasFrequencyContext = canvasFrequency.getContext("2d");
+          const canvasVolumeContext = canvasVolume.getContext("2d");
+          const barHeight = (canvasFrequency.height / bufferLength) * 2.5;
+          const gradient = canvasVolumeContext.createLinearGradient(0, 0, canvasVolume.width, 0);
+          const barMinWidth = 5;
+          var barWidth;
+          var y = 0;
+          var volumeValue;
+
+          canvasFrequencyContext.clearRect(0, 0, canvasFrequency.width, canvasFrequency.height);
+          canvasVolumeContext.clearRect(0, 0, canvasFrequency.width, canvasFrequency.height);
+
+          drawVisual = requestAnimationFrame(draw);
+          analyser.getByteFrequencyData(dataArray);
+
+          for (var i = 0; i < bufferLength; i++) {
+            barWidth = dataArray[i] + barMinWidth;
+            canvasFrequencyContext.fillStyle = interfaceColor;
+            canvasFrequencyContext.fillRect(canvasFrequency.width - barWidth / 2, y, barWidth, barHeight);
+            y += barHeight + 1;
+          };
+
+
+          volumeValue = Math.max.apply(Math, dataArray) / 2;
+          gradient.addColorStop(0, interfaceColor);
+          gradient.addColorStop(1, interfaceColorRGBA);
+          canvasVolumeContext.fillStyle = gradient;
+          canvasVolumeContext.fillRect(0, 0, volumeValue, canvasVolume.height);
+
+        };
+
+        draw();
+
+        source.connect(filter);
+        filter.connect(analyser);
+        analyser.connect(destination);
+      })
+      .catch(function(err) {
+        console.log("The following error occured: " + err);
+      });
+  } else {
+    alert("Your browser not supported getUserMedia!");
+  };
+})();
