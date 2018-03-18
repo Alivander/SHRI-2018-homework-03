9b51bb87918832273e0cdf9a1f500093b9d702fe
Alina Vanieva <vanieva.alina@yandex.ru>
2018-03-18 17:49:51 +0300

    Updates

--- js/script.js
+++ js/script.js
@@ -77,19 +77,15 @@
   } else {
     navigator.mediaDevices.getUserMedia = function(constraints) {
 
-      // First get ahold of the legacy getUserMedia, if present
       var getUserMedia = navigator.webkitGetUserMedia || navigator.mozGetUserMedia;
 
-      // Some browsers just don't implement it - return a rejected promise with an error
-      // to keep a consistent interface
       if (!getUserMedia) {
         return Promise.reject(new Error('getUserMedia is not implemented in this browser'));
       }
 
-      // Otherwise, wrap the call to the old navigator.getUserMedia with a Promise
       return new Promise(function(resolve, reject) {
         getUserMedia.call(navigator, constraints, resolve, reject);
       });
-    }
+    };
   };
 })();
